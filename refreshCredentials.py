1a59caaa00632d707279cad662f0ef81800d7a36
TZFC <51830440+TZFC@users.noreply.github.com>
2023-11-19 19:41:41 +0800

    fix abs path when run in cron

--- refreshCredentials.py
+++ refreshCredentials.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 from bilibili_api import sync
 from mysql.connector import connect
@@ -7,13 +8,15 @@ from CredentialGetter import getCredential
 
 # from Utils.EmailSender import sendEmail
 
+path = os.getcwd()
+
 # Hard coding 'tzfc' for now. Will iterate through user list when needed
 credential = getCredential("tzfc")
 if sync(credential.check_refresh()):
     sync(credential.refresh())
     val = (credential.sessdata, credential.bili_jct, credential.buvid3, credential.ac_time_value)
     sql = "UPDATE credentials SET sessdata = %s, bili_jct = %s, buvid3 = %s, ac_time_value = %s WHERE user = 'tzfc'"
-    with connect(**json.load(open("./mysql.json"))) as mydb:
+    with connect(**json.load(open(os.path.join(path, "mysql.json")))) as mydb:
         with mydb.cursor() as cursor:
             cursor.execute(sql, val)
             mydb.commit()
