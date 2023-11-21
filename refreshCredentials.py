8d8df3571f9a1e79fbd60036563bd61371a89412
TZFC <51830440+TZFC@users.noreply.github.com>
2023-11-21 12:39:13 +0800

    multiple masters supported

--- refreshCredentials.py
+++ refreshCredentials.py
@@ -1,4 +1,4 @@
-import json
+from json import load
 import os
 
 from bilibili_api import sync
@@ -11,12 +11,14 @@ from CredentialGetter import getCredential
 path = os.getcwd()
 
 # Hard coding 'tzfc' for now. Will iterate through user list when needed
-credential = getCredential("tzfc")
-if sync(credential.check_refresh()):
-    sync(credential.refresh())
-    val = (credential.sessdata, credential.bili_jct, credential.buvid3, credential.ac_time_value)
-    sql = "UPDATE credentials SET sessdata = %s, bili_jct = %s, buvid3 = %s, ac_time_value = %s WHERE user = 'tzfc'"
-    with connect(**json.load(open(os.path.join(path, "mysql.json")))) as mydb:
-        with mydb.cursor() as cursor:
-            cursor.execute(sql, val)
-            mydb.commit()
+masterConfig = load(open(os.path.join(path, "Configs/masterConfig.json")))
+for master in masterConfig["masters"]:
+    credential = getCredential(master)
+    if sync(credential.check_refresh()):
+        sync(credential.refresh())
+        sql = "UPDATE credentials SET sessdata = %s, bili_jct = %s, buvid3 = %s, ac_time_value = %s WHERE user = %s"
+        val = (credential.sessdata, credential.bili_jct, credential.buvid3, credential.ac_time_value. master)
+        with connect(**load(open(os.path.join(path, "mysql.json")))) as mydb:
+            with mydb.cursor() as cursor:
+                cursor.execute(sql, val)
+                mydb.commit()
