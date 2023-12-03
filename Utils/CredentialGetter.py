a10d20772c8330d79711392033411128f1f93c86
TZFC <51830440+TZFC@users.noreply.github.com>
2023-12-03 16:24:18 +0800

    try fix cannot find mysql.json

--- Utils/CredentialGetter.py
+++ Utils/CredentialGetter.py
@@ -5,7 +5,7 @@ from mysql.connector import connect
 
 
 def get_credential(master: str) -> Credential:
-    with connect(**load(open("../Configs/mysql.json"))) as mydb:
+    with connect(**load(open("Configs/mysql.json"))) as mydb:
         with mydb.cursor() as cursor:
             cursor.execute(
                 f"SELECT sessdata, bili_jct, buvid3, ac_time_value FROM credentials WHERE master = '{master}'")
