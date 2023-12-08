adff7024980340576a95e5ee277369f124432626
TZFC <51830440+TZFC@users.noreply.github.com>
2023-12-08 19:49:22 +0800

    record last 10 checkin

--- Utils/Checkin.py
+++ Utils/Checkin.py
@@ -1,14 +1,38 @@
 from datetime import datetime
 
 
-async def record_checkin(start_time: datetime, end_time: datetime, room_id: int, database):
-    sql = "SELECT DISTINCT uid FROM danmu WHERE room_id = %s AND time BETWEEN %s AND %s"
-    val = (room_id, start_time, end_time)
+async def record_checkin(start_time: datetime, end_time: datetime, master: str, room_id: int, database):
     with database.cursor() as cursor:
+        sql = "SELECT dedeuserid FROM credentials WHERE master = %s"
+        val = (master,)
         cursor.execute(sql, val)
-        checker = cursor.fetchall()
-    sql = "INSERT INTO checkin (uid, room_id, attend) VALUES (%s, %s, %s) ON DUPLICATE KEY UPDATE attend = attend + 1"
-    val = [(uid[0], room_id, 1) for uid in checker]
-    with database.cursor() as cursor:
+        dedeuserid = cursor.fetchall()[0][0]
+
+        sql = "SELECT slot_0, slot_1, slot_2, slot_3, slot_4, slot_5, slot_6, slot_7, slot_8, slot_9 WHERE uid = %s AND room_id = %s"
+        val = (dedeuserid, room_id)
+        cursor.execute(sql, val)
+        slots: tuple = cursor.fetchall()[0]
+        head = slots.index(1)
+        next_head = head + 1 if head != 9 else 0
+
+        sql = "SELECT DISTINCT uid FROM danmu WHERE room_id = %s AND time BETWEEN %s AND %s"
+        val = (room_id, start_time, end_time)
+        cursor.execute(sql, val)
+        result = cursor.fetchall()
+        unique_uid = [item[0] for item in result]
+        if dedeuserid in unique_uid:
+            unique_uid.remove(dedeuserid)
+
+        sql = f"UPDATE checkin SET slot_{head} = 0 where room_id = %s"
+        val = (room_id,)
+        cursor.execute(sql, val)
+
+        sql = f"INSERT INTO checkin (room_id, uid, slot_{head}) VALUES(%s, %s, 1) ON DUPLICATE KEY UPDATE slot_{head} = 1"
+        val = [(room_id, uid) for uid in unique_uid]
         cursor.executemany(sql, val)
+
+        sql = f"UPDATE checkin SET slot_{next_head} = 1 where room_id = %s AND uid = %s"
+        val = (room_id, dedeuserid)
+        cursor.execute(sql, val)
+
     database.commit()
