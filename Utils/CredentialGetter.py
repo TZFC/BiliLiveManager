bd7181152a67ffe682c71e4efe25784e650ff11e
TZFC <51830440+TZFC@users.noreply.github.com>
2023-12-08 19:34:27 +0800

    include dedeuserid in credential

--- Utils/CredentialGetter.py
+++ Utils/CredentialGetter.py
@@ -9,6 +9,6 @@ def get_credential(master: str) -> Credential:
         with mydb.cursor() as cursor:
             cursor.execute(
                 f"SELECT sessdata, bili_jct, buvid3, ac_time_value FROM credentials WHERE master = '{master}'")
-            sess, jct, bd3, act = cursor.fetchone()
-    credential = Credential(sessdata=sess, bili_jct=jct, buvid3=bd3, ac_time_value=act)
+            sessdata, bili_jct, buvid3, ac_time_value, dedeuserid = cursor.fetchone()
+    credential = Credential(sessdata=sessdata, bili_jct=bili_jct, buvid3=buvid3, ac_time_value=ac_time_value, dedeuserid=dedeuserid)
     return credential
