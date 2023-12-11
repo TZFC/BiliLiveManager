2ee389d7d8d72ac93e9127649accbca3f7f9484d
TZFC <51830440+TZFC@users.noreply.github.com>
2023-12-11 17:34:34 +0800

    exclude non text type when checkin

--- master.py
+++ master.py
@@ -80,11 +80,12 @@ def bind(room: LiveDanmaku):
         room_id = event['room_display_id']
         received_uid = event["data"]["info"][SENDER_INFO_IDX][SENDER_UID_IDX]
         text = event["data"]["info"][TEXT_IDX]
+        message_type = event["data"]["info"][0][MSG_TYPE_IDX]
 
         # 封禁关键词
         if roomConfigs[room_id]["feature_flags"]["unban"]:
             await ban_on_keyword(text=text,
-                                 message_type=event["data"]["info"][0][MSG_TYPE_IDX],
+                                 message_type=message_type,
                                  received_uid=received_uid,
                                  room_id=room_id,
                                  live_room=liveRooms[room_id],
@@ -102,6 +103,7 @@ def bind(room: LiveDanmaku):
                              medal_room=medal_room,
                              medal_level=medal_level,
                              text=text,
+                             message_type=message_type,
                              room_id=room_id,
                              database=mydb)
 
