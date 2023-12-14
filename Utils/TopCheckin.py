fafd7043a05788cce0d5caf19b30147dbbc5df51
TZFC <51830440+TZFC@users.noreply.github.com>
2023-12-15 01:00:23 +0800

    allow custom top_k

--- Utils/TopCheckin.py
+++ Utils/TopCheckin.py
@@ -1,4 +1,4 @@
-async def top_checkin(master: str, room_id: int, database) -> list:
+async def get_top_k_checkin(master: str, room_id: int, database, top_k: int) -> list:
     with database.cursor() as cursor:
         sql = "SELECT dedeuserid FROM credentials WHERE master = %s"
         val = (master,)
@@ -7,8 +7,8 @@ async def top_checkin(master: str, room_id: int, database) -> list:
 
         sql = ("SELECT uid, count FROM checkin "
                f"WHERE room_id = %s AND uid <> %s "
-               f"ORDER BY count DESC LIMIT 10")
-        val = (room_id, dedeuserid)
+               f"ORDER BY count DESC LIMIT %s")
+        val = (room_id, dedeuserid, top_k)
         cursor.execute(sql, val)
         result = cursor.fetchall()
     return result
