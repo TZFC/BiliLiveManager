792558e1391eb5d976a900f7186e7d7f1f2105f6
TZFC <51830440+TZFC@users.noreply.github.com>
2023-12-17 18:05:28 +0800

    offload ludeng ident to MYSQL

--- Utils/Summarizer.py
+++ Utils/Summarizer.py
@@ -1,16 +1,9 @@
+import os
 from datetime import datetime, timedelta
 from json import load
 
 OFFSET = timedelta(seconds=30)
-
-
-def isDeng(row: dict, field_index: dict, room_id: int, room_config) -> bool:
-    text = row[field_index["text"]]
-    keywords = room_config["keyword"]
-    if text[:2] in keywords:
-        return True
-    else:
-        return False
+path = os.getcwd()
 
 
 def summarize(room_id: int, database) -> (str, str, datetime, datetime):
@@ -22,29 +15,31 @@ def summarize(room_id: int, database) -> (str, str, datetime, datetime):
         if not result:
             return None, None, None, None
         start_time, end_time = result[0]
-        cursor.execute("SELECT * FROM danmu WHERE room_id = %s AND time BETWEEN %s AND %s",
+
+        # 找出路灯
+        with open(os.path.join(path, f"Configs/config{room_id}.json")) as roomConfigFile:
+            room_config = load(roomConfigFile)
+        regex = '|'.join(['^' + keyword for keyword in room_config['keyword']])
+        cursor.execute(f"SELECT time, SUBSTRING(text, 3), name, uid FROM danmu "
+                       f"WHERE room_id = %s AND time BETWEEN %s AND %s "
+                       f"AND text RLIKE '{regex}'",
                        (room_id, start_time, end_time))
         raw_danmu = cursor.fetchall()
-        field_index = {field_name: index for index, field_name in enumerate(cursor.column_names)}
 
-    # 找出路灯关键词
-    room_config = load(open(f"Configs/config{room_id}.json"))
-    email_rows, jump_rows = list(zip(*[
-        (
-            # email fields
-            "\t".join([
-                str(row[field_index["time"]] - OFFSET),  # 时间
-                row[field_index["text"]][2:],  # 去除指令词的路灯内容
-                row[field_index["name"]],  # 发送者用户名
-                str(row[field_index["uid"]])  # 发送者uid
-            ]),
-            # jump fields
-            " ".join([
-                str(row[field_index["time"]] - start_time - OFFSET),  # 相对开播时间
-                row[field_index["text"]][2:]  # 去除指令词的路灯内容
-            ])
-        ) for row in raw_danmu if isDeng(row, field_index, room_id, room_config)
-    ])) or ([], [])
+    email_rows = []
+    jump_rows = []
+    for row in raw_danmu:
+        email_rows.append("\t".join([
+            str(row[0]),  # 时间
+            row[1],  # 去除指令词的路灯内容
+            row[2],  # 发送者用户名
+            str(row[3]),  # 发送者uid
+        ]))
+        jump_rows.append(" ".join([
+            str(row[0] - start_time - OFFSET),  # 相对开播时间
+            row[1],  # 去除指令词的路灯内容
+        ]))
+
     email_text = "\n".join(email_rows)
     jump_text = "\n".join(jump_rows)
 
