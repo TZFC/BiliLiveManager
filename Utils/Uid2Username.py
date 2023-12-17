c8edef93b2e51132f7f7ccdaea76f883d63ac76a
TZFC <51830440+TZFC@users.noreply.github.com>
2023-12-17 19:39:49 +0800

    fix username not given

--- Utils/Uid2Username.py
+++ Utils/Uid2Username.py
@@ -1,7 +1,11 @@
 from bilibili_api.user import User
+from asyncio import sleep
 
 
 async def uid2username(uid_count):
     user = User(uid=uid_count[0])
     info = await user.get_user_info()
+    if not info['name']:
+        await sleep(1)
+        info = await user.get_user_info()
     return info['name'], uid_count[1]
