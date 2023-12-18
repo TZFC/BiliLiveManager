9ccde892dcc84ff791a5964e429c4de0fb9a4165
TZFC <51830440+TZFC@users.noreply.github.com>
2023-12-18 12:40:08 +0800

    use dedeuserid from credential instead of database query

--- EventHandler/PREPARING_handler.py
+++ EventHandler/PREPARING_handler.py
@@ -42,8 +42,8 @@ async def handle_preparing(event, database, master_config, live_room, room_confi
                                  room_id=room_id,
                                  checkin_days=room_config['checkin_days'],
                                  database=database)
-            top_uid_count = await get_top_k_checkin(master=room_config['master'], room_id=room_id, database=database,
-                                                    top_k=10)
+            top_uid_count = await get_top_k_checkin(master_uid=room_config['master_credential'].dedeuserid,
+                                                    room_id=room_id, database=database, top_k=10)
             top_username_count = await asyncio.gather(*map(uid2username, top_uid_count))
             await update_page(target=f"/var/www/html/{room_id}.html",
                               checkin_days=room_config['checkin_days'],
--- Utils/TopCheckin.py
+++ Utils/TopCheckin.py
@@ -1,14 +1,9 @@
-async def get_top_k_checkin(master: str, room_id: int, database, top_k: int) -> list:
+async def get_top_k_checkin(master_uid: int, room_id: int, database, top_k: int) -> list:
     with database.cursor() as cursor:
-        sql = "SELECT dedeuserid FROM credentials WHERE master = %s"
-        val = (master,)
-        cursor.execute(sql, val)
-        dedeuserid = int(cursor.fetchall()[0][0])
-
         sql = ("SELECT uid, count FROM checkin "
                f"WHERE room_id = %s AND uid <> %s "
                f"ORDER BY count DESC LIMIT %s")
-        val = (room_id, dedeuserid, top_k)
+        val = (room_id, master_uid, top_k)
         cursor.execute(sql, val)
         result = cursor.fetchall()
     return result
