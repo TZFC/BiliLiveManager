09dfe6aaaefcd574095b33d625205e3395c43a16
TZFC <51830440+TZFC@users.noreply.github.com>
2023-12-18 15:42:02 +0800

    Disambiguation: rename roomConfigs to roomInfo, containing a key called room_config holding the actual configs

--- EventHandler/DANMU_MSG_handler.py
+++ EventHandler/DANMU_MSG_handler.py
@@ -11,22 +11,22 @@ from Utils.Uid2Username import uid2username
 from web.UpdatePage import update_page
 
 
-async def handle_danmu_msg(event, database, master_config, live_room, room_config, credential):
+async def handle_danmu_msg(event, database, master_config, room_info):
     room_id = event['room_display_id']
     received_uid = event["data"]["info"][SENDER_INFO_IDX][SENDER_UID_IDX]
     text = event["data"]["info"][TEXT_IDX]
     message_type = event["data"]["info"][0][MSG_TYPE_IDX]
-    streamer_uid = await live_room._LiveRoom__get_ruid()
+    streamer_uid = await room_info['live_room']._LiveRoom__get_ruid()
 
     async with (asyncio.TaskGroup() as tg):
         # 主播及master指令
-        if received_uid in {int(credential.dedeuserid), streamer_uid}:
+        if received_uid in {int(room_info['credential'].dedeuserid), streamer_uid}:
             if "checkin" in text:
-                info = await live_room.get_room_info()
+                info = await room_info.get_room_info()
                 live_status = info['room_info']['live_status']
                 if live_status == LIVE_STATUS_STREAMING \
-                        and room_config["feature_flags"]["checkin"] \
-                        and not room_config['state']['pre-checkin']:
+                        and room_info['room_config']["feature_flags"]["checkin"] \
+                        and not room_info['state']['pre-checkin']:
                     with database.cursor() as cursor:
                         sql = "SELECT start FROM liveTime WHERE room_id = %s AND end IS NULL AND summary IS NULL"
                         val = (room_id,)
@@ -35,26 +35,27 @@ async def handle_danmu_msg(event, database, master_config, live_room, room_confi
                     # 统计直播间发言人
                     await record_checkin(start_time=start_time,
                                          end_time=datetime.fromtimestamp(event['data']["info"][TIMESTAMP_IDX]['ts']),
-                                         master=room_config['master'],
+                                         master=room_info['room_config']['master'],
                                          room_id=room_id,
-                                         checkin_days=room_config['checkin_days'],
+                                         checkin_days=room_info['room_config']['checkin_days'],
                                          database=database)
-                    top_uid_count = await get_top_k_checkin(master_uid=credential.dedeuserid,
+                    top_uid_count = await get_top_k_checkin(master_uid=room_info['credential'].dedeuserid,
                                                             room_id=room_id, database=database, top_k=10)
                     top_uid_username_count = await asyncio.gather(*map(uid2username, top_uid_count))
-                    tg.create_task(send_report_checkin(live_room=live_room, top_uid_username_count=top_uid_username_count))
+                    tg.create_task(send_report_checkin(live_room=room_info['live_room'],
+                                                       top_uid_username_count=top_uid_username_count))
                     tg.create_task(update_page(target=f"/var/www/html/{room_id}.html",
-                                               checkin_days=room_config['checkin_days'],
+                                               checkin_days=room_info['room_config']['checkin_days'],
                                                content=top_uid_username_count))
-                    room_config['state']['pre-checkin'] = True
+                    room_info['state']['pre-checkin'] = True
         # 封禁关键词
-        if room_config["feature_flags"]["unban"]:
+        if room_info['room_config']["feature_flags"]["unban"]:
             tg.create_task(ban_on_keyword(text=text,
                                           message_type=message_type,
                                           received_uid=received_uid,
                                           room_id=room_id,
-                                          live_room=live_room,
-                                          room_config=room_config,
+                                          live_room=room_info['live_room'],
+                                          room_config=room_info['room_config'],
                                           database=database))
         # 记录弹幕
         try:
