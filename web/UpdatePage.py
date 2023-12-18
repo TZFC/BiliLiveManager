83a43915f1080f715ba2da112aded84cdf19ea33
TZFC <51830440+TZFC@users.noreply.github.com>
2023-12-18 17:54:54 +0800

    add top class to web

--- web/UpdatePage.py
+++ web/UpdatePage.py
@@ -1,7 +1,7 @@
 async def update_page(target, checkin_days, content):
-    new_content = "\n".join(
-        [f"<p class=_{count} id=_{rank + 1}>{username if username else uid} {count}/{checkin_days}</p>" for
-         rank, (uid, username, count) in enumerate(content)])
+    highest_count = content[0][-1]
+    new_content = "\n".join([f'''<p class="{'top' if count == highest_count else "not_top"} {count}" id=_{rank + 1}>{username if username else uid} {count}/{checkin_days}</p>'''
+                             for rank, (uid, username, count) in enumerate(content)])
     with open("web/ROOM_ID.html") as file:
         template = file.read()
     before, after = template.split("<p>")
