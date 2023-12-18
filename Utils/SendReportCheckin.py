7178463c8d01bd652adb7e1448a04c0e3ba10fe7
TZFC <51830440+TZFC@users.noreply.github.com>
2023-12-18 14:46:16 +0800

    sleep first in case master is initiating the checkin request

--- Utils/SendReportCheckin.py
+++ Utils/SendReportCheckin.py
@@ -5,5 +5,5 @@ from bilibili_api import Danmaku
 
 async def send_report_checkin(live_room, top_uid_username_count):
     for rank, (uid, username, count) in enumerate(top_uid_username_count):
-        await live_room.send_danmaku(Danmaku(f"第{rank+1}名 {username} 打卡{count}次"), reply_mid=uid)
         await asyncio.sleep(5)
+        await live_room.send_danmaku(Danmaku(f"第{rank+1}名 {username} 打卡{count}次"), reply_mid=uid)
