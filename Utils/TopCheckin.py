a9c74e3a88862441e82df459f8930a8ee8b40874
TZFC <51830440+TZFC@users.noreply.github.com>
2023-12-18 18:25:25 +0800

    fix: misconfigured sql

--- Utils/TopCheckin.py
+++ Utils/TopCheckin.py
@@ -11,7 +11,7 @@ async def get_top_k_checkin(master_uid: int, room_id: int, database, top_k: int)
 async def get_top_checkin(master_uid: int, room_id: int, database) -> list:
     with database.cursor() as cursor:
         sql = ("SELECT uid, count FROM checkin "
-               f"WHERE room_id = %s AND uid <> %s AND count > 0"
+               f"WHERE room_id = %s AND uid <> %s AND count > 0 "
                f"ORDER BY count DESC")
         val = (room_id, master_uid)
         cursor.execute(sql, val)
