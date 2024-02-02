3c312b92389243ba82ff29a0a57df9d80a3074d5
TZFC <tianzifangchen@gmail.com>
2024-02-02 11:41:48 -0500

    feat: report checkin count in page

--- web/UpdatePage.py
+++ web/UpdatePage.py
@@ -1,6 +1,6 @@
 async def update_page(target, checkin_days, content):
     highest_count = content[0][-1]
-    new_content = []
+    new_content = [f'<p class="count">过去{checkin_days}场直播内{len(content)}人打卡</p>']
     for rank, (uid, username, count) in enumerate(content):
         top_class = 'top' if count == highest_count else 'not_top'
         count_class = f"_{count}"
