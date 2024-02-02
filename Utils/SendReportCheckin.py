e7181395bcaf1a04a97e6dc7a5cca2f9b8b25b41
TZFC <tianzifangchen@gmail.com>
2024-02-02 10:19:46 -0500

    fix: when danmu is exactly len=20, send only one msg

--- Utils/SendReportCheckin.py
+++ Utils/SendReportCheckin.py
@@ -8,6 +8,6 @@ async def send_report_checkin(live_room, top_uid_username_count):
         if rank >= 10:
             break
         text = f"第{rank + 1}名 {username} 打卡{count}次"
-        for chunk in range(len(text) // 20 + 1):
+        for chunk in range((len(text) - 1) // 20 + 1):
             await asyncio.sleep(5)
             await live_room.send_danmaku(Danmaku(text[chunk * 20:(chunk + 1) * 20]), reply_mid=uid)
