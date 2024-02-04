774124fac3ec2858eb6718673d48250d211e505b
TZFC <tianzifangchen@gmail.com>
2024-02-04 01:34:09 -0500

    feat: report checkin count in report checkin

--- Utils/SendReportCheckin.py
+++ Utils/SendReportCheckin.py
@@ -11,3 +11,4 @@ async def send_report_checkin(live_room, top_uid_username_count):
         for chunk in range((len(text) - 1) // 20 + 1):
             await asyncio.sleep(5)
             await live_room.send_danmaku(Danmaku(text[chunk * 20:(chunk + 1) * 20]), reply_mid=uid)
+    await live_room.send_danmaku(Danmaku(f"过去30场内，共计{len(top_uid_username_count)}人打卡"))
