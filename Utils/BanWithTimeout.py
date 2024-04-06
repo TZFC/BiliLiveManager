e9dd8927f0704e59983221e67a82340fd320803d
TZFC <tianzifangchen@gmail.com>
2024-04-06 01:46:36 -0400

    report ban on first offense

--- Utils/BanWithTimeout.py
+++ Utils/BanWithTimeout.py
@@ -2,15 +2,31 @@ import asyncio
 
 from bilibili_api.exceptions import ResponseCodeException
 from bilibili_api.live import LiveRoom
+from bilibili_api import Danmaku
 
 
-async def ban_with_timeout(live_room: LiveRoom, uid: int, timeout: int, database):
+async def ban_with_timeout(live_room: LiveRoom, uid: int, offense: tuple, database):
     try:
         await live_room.ban_user(uid)
+        select_sql = "SELECT * FROM ban_history WHERE uid = %s and room_id = %s"
+        insert_sql = "INSERT INTO ban_history (uid, room_id) VALUES (%s, %s)"
+        val = (uid, live_room.room_display_id)
+        with database.cursor() as cursor:
+            cursor.execute(select_sql, val)
+            result = cursor.fetchone()
+        if not result:
+            if offense[1] % 60 == 0:
+                await live_room.send_danmaku(
+                    Danmaku(f"发{offense[0]}会被禁言{offense[1] / 60}分，送{offense[2]}提前解禁"), reply_mid=uid)
+            else:
+                await live_room.send_danmaku(
+                    Danmaku(f"发{offense[0]}会被禁言{offense[1] / 60:.1f}分，送{offense[2]}提前解禁"), reply_mid=uid)
+            cursor.execute(insert_sql, val)
+            database.commit()
     except Exception as e:
         print(f"ban failed for {uid}")
         print(e)
-    await asyncio.sleep(timeout)
+    await asyncio.sleep(offense[1])
     try:
         await live_room.unban_user(uid)
     except ResponseCodeException:
