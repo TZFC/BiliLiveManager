cabf6cc0b199781b9606085593cebbbfa13824ef
TZFC <tianzifangchen@gmail.com>
2024-05-09 01:19:30 -0400

    fix race condition on database result use

--- Utils/UnbanAll.py
+++ Utils/UnbanAll.py
@@ -21,18 +21,15 @@ async def unban_all(live_room: LiveRoom, database):
     with database.cursor() as cursor:
         cursor.execute(sql, val)
     result = cursor.fetchall()
-    if not result:
-        return
-    else:
-        for uid, time in result:
-            if time < datetime.now():
-                print(f"{uid} not properly released. Releasing at live end.")
-            try:
-                await unban_retry(live_room, uid)
-            except ResponseCodeException as e:
-                print(f"unban failed for {uid}")
-                print(e)
-            sql = "DELETE FROM banned WHERE uid=%s AND room_id=%s"
-            val = (uid, live_room.room_display_id)
-            with database.cursor() as cursor:
-                cursor.execute(sql, val)
+    for uid, time in result:
+        if time < datetime.now():
+            print(f"{uid} not properly released. Releasing at live end.")
+        try:
+            await unban_retry(live_room, uid)
+        except ResponseCodeException as e:
+            print(f"unban failed for {uid}")
+            print(e)
+        sql = "DELETE FROM banned WHERE uid=%s AND room_id=%s"
+        val = (uid, live_room.room_display_id)
+        with database.cursor() as cursor:
+            cursor.execute(sql, val)
